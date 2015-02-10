590c14065b441608e1600a80a9ba24918287b64d
arriceclaimer <darklord_nekiy1@ukr.net>
2015-02-10 16:06:42 +0200

    created project dir
    first changes

---
+++ onota/onota/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  onota
+//
+//  Created by Admin on 10.02.15.
+//  Copyright (c) 2015 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
